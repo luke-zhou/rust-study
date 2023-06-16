2bdde97bfc7ee2726bb0011be5c7134e5d475d2c
Luke Zhou <sharlock@gmail.com>
2023-06-16 10:09:18 +1000

    create string compression

---
+++ algorithm/src/string_compression.rs
@@ -0,0 +1,32 @@
+/*
+String Compression: Implement a method to perform basic string compression using the counts
+of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+"compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+ */
+pub fn run(s: &str) -> String{
+    if s.len() == 0 {
+        return String::new()
+    }
+    let mut result = String::new();
+    let mut c = s.chars().nth(0).unwrap();
+    let mut count = 1;
+    for i in s[1..].chars(){
+        if i == c {
+            count +=1;
+        }else{
+            result.push(c);
+            result.push_str(&count.to_string());
+            count = 1;
+            c = i;
+        }
+    }
+    result.push(c);
+    result.push_str(&count.to_string());
+
+    if result.len() >= s.len() {
+        String::from(s)
+    }else{
+        result
+    }
+}
