ef5eba1ffef07ceba35f1ffa3ea487152365da1a
Luke Zhou <sharlock@gmail.com>
2023-07-10 12:02:23 +1000

    create remove dups for linked list

--- algorithm/src/remove_dups.rs
+++ algorithm/src/remove_dups.rs
@@ -1,14 +1,44 @@
 /*
 Remove Dups: Write code to remove duplicates from an unsorted linked list.
 */
-use std::collections::{HashSet, LinkedList};
-use std::hash::Hash;
-use std::fmt::Display;
+use crate::linked_list::*;
+use std::collections::HashSet;
 
-pub fn run<T>(list: &mut LinkedList<T>)
+pub fn run(list: &mut LinkedList)
 {
-    let mut h: HashSet<&T> = HashSet::new();
-    
+    let mut h: HashSet<i32> = HashSet::new();
+    h.insert(list.value);
+    delete_dup(list, &mut h);
+}
+
+fn delete_dup(list: &mut LinkedList, dups: &mut HashSet<i32>){
+    // println!();
+    // println!("l: {list}");
+    // println!("h: {dups:?}");
+
+    if let ListNode::Node(l) = &mut list.next{
+        // println!("v:{}", l.value);
+        if dups.contains(&l.value){
+            list.next = l.next.clone();
+            delete_dup(list, dups);
+        }else{
+            dups.insert(l.value);
+            delete_dup(l, dups);
+        }
+    }
+}
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_remove_dups() {
+        let l: LinkedList = LinkedList::new(1);
+        let mut l2 = l.prepend(1).prepend(2).prepend(3).prepend(2);
+        println!("{l2}");
+        run(&mut l2);
+        println!("{l2}");
+    }
 }
